d88d32511e8cc5f0a2f7bdf77f0e9cc0e698d94b
ひかり <hikari.photon+dev@gmail.com>
2025-11-29 16:23:29 +0900

    インストールスクリプトの更新

--- setup.ps1
+++ setup.ps1
@@ -1,36 +1,49 @@
 $DIR = Join-Path $HOME "genimage"
 
 if (-not (Get-Command uv -ErrorAction SilentlyContinue)) {
-    Write-Error("❌ uv コマンドが存在しません")
-    Write-Error("")
-    Write-Error("uv コマンドのインストールが必要です。")
-    Write-Error("以下のコマンドを実行してインストールしてください。")
-    Write-Error("")
-    Write-Error('PS> powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"')
-    Write-Error("")
+    Write-Host("❌ uv コマンドが存在しません") -ForegroundColor Red
+    Write-Host("") -ForegroundColor Red
+    Write-Host("uv コマンドのインストールが必要です。") -ForegroundColor Red
+    Write-Host("以下のコマンドを実行してインストールしてください。") -ForegroundColor Red
+    Write-Host("") -ForegroundColor Red
+    Write-Host('PS> powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"') -ForegroundColor Red
+    Write-Host("") -ForegroundColor Red
+    exit 1
+}
+
+if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+    Write-Host("❌ git コマンドが存在しません") -ForegroundColor Red 
+    Write-Host("") -ForegroundColor Red
+    Write-Host("以下のコマンドを実行してインストールしてください。") -ForegroundColor Red
+    Write-Host("") -ForegroundColor Red
+    Write-Host("PS> winget install --id Git.Git") -ForegroundColor Red
+    Write-Host("") -ForegroundColor Red
     exit 1
 }
 
 if (Test-Path $DIR) {
-    Write-Error("❌ $DIR は既に存在します")
-    exit 1
+    Push-Location .
+    Set-Location $DIR
+    git pull
+    Pop-Location
+    exit 0
 }
 
 git clone https://github.com/Himeyama/genimage.git $DIR
 if ($LASTEXITCODE -ne 0) {
-    Write-Error("❌ git clone に失敗しました")
+    Write-Host("❌ git clone に失敗しました") -ForegroundColor Red
     exit 1
 }
 
 uv venv --directory $DIR
 if ($LASTEXITCODE -ne 0) {
-    Write-Error("❌ uv venv に失敗しました")
+    Write-Host("❌ uv venv に失敗しました") -ForegroundColor Red
     exit 1
 }
 
 uv sync --directory $DIR
 if ($LASTEXITCODE -ne 0) {
-    Write-Error("❌ uv sync に失敗しました")
+    Write-Host("❌ uv sync に失敗しました") -ForegroundColor Red
     exit 1
 }
 
